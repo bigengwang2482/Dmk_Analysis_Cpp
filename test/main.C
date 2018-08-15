f15b46a95da94b5a32affaad37f0fc70eab6950f
Bigeng Wang <bw2482@columbia.edu>
2018-08-15 14:51:13 -0400

    Test file list

--- test/main.C
+++ test/main.C
@@ -8,13 +8,21 @@ int main()
 {
 	std::vector<double> pion;
 	std::vector<std::string> config;
+	std::vector<std::string> file_list;
+	std::string marker="*";
+	std::string common_name="./inputs/traj_*_pion_1e-4.txt";
 	std::string config_path="./inputs/list.txt";
 	std::string path_pion="../traj_2840_pion_1e-4.txt";
-	list_config( config, config_path);	
+	list_config( config, config_path);
+	list_file_name(file_list, config, common_name, marker );		
 	meson_in(pion, path_pion);	 
 	//Display
 	std::cout << "The vector pion contents: " << std::endl;
 	for (auto i=pion.begin(); i!= pion.end() ; ++i)
+		std::cout << *i << '\n';
+	
+	std::cout << "The file list contents: " << std::endl;
+	for (auto i=file_list.begin(); i!= file_list.end() ; ++i)
 		std::cout << *i << '\n';
 	
 
